4a1440c86a94dc1cb03d3befd8f1cbbba3cdcf3b
Korman <korman@qq.com>
2022-10-19 11:08:39 +0800

    update

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,2 +1,3 @@
 protoc --go_out=./gate ./pb/gate/*.proto
+protoc --go_out=./grpc_server ./pb/grpc_server/*.proto
 protoc --go-grpc_out=./grpc_server ./pb/grpc_server/*.proto
--- go_user.go
+++ go_user.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
+	"net"
 
+	"go-test-user/grpc_server"
+
+	"google.golang.org/grpc"
+)
+
+type UserCenterServerImp struct {
+	grpc_server.UnimplementedUserCenterServer
+}
+
+func (m *UserCenterServerImp) DoTest(context context.Context, req *grpc_server.DoTestReq) (*grpc_server.DoTestResp, error) {
+	println(req.Text)
+	return nil, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":9899")
+
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	userCenterServer := &UserCenterServerImp{}
+
+	grpc_server.RegisterUserCenterServer(s, userCenterServer)
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
